b31b2061c28b14ecebcc88cb4810bc67d673d88f
LPMazzochi <leonardo.mazzochi@hotmail.com>
2025-02-17 11:20:45 -0300

    adicionado ola mundo

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Meu primeiro código php, <strong>Ketilin esta com o joelho dolorido</strong>";
+echo "Meu primeiro código php  olá mundo, <strong>Ketilin esta com o joelho dolorido</strong>";
 
 ?>
